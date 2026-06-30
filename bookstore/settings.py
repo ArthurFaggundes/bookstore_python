d9a812f1d06398cb49fce18b363c491d76f0b151
Arthur Faggundês <arthurhfagundes6@gmail.com>
2026-06-30 17:34:52 -0300

    Adicionei novos testes para o modelo Order e Product, implementei CategoryViewSet e atualizei configurações do projeto

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     'rest_framework',
-    'django_extensions'
+    'django_extensions',
+    'rest_framework.authtoken',
     'order',
     'product',
 ]
